b76a54ae39aeaa12c4f6403ccbc5866e04dbb67b
i7677264 <i7677264@bournemouth.ac.uk>
2015-02-26 10:46:53 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Project</title>
+        <link href="css/normal.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+    </head>
+    <body>
