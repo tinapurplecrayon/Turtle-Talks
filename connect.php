ee1a7b58de11ca2e82ffa8871c724788bc551896
i7677264 <i7677264@bournemouth.ac.uk>
2015-03-10 15:20:48 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,15 +1,16 @@
 <?php
     define("DB_SERVER", "localhost");
-    define("DB_USER", "dbuttner");
-    define("DB_PASS", "CgQC5QKJgGo=");
-    define("DB_NAME", "dbuttner"); // when using dakar, this will be the same as your username
+    define("DB_USER", "root"); 
+    define("DB_PASS", "password"); 
+    define("DB_NAME", "test2"); 
 
-    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
-    if(mysqli_connect_errno()) {
-        die("Database connection failed: " . 
+    if(mysqli_connect_errno()) { 
+        die("Database connection failed: " .
         mysqli_connect_error() . 
         " (" . mysqli_connect_errno() . ")"
         );
     }
 ?>
+
