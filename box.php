f9b248108cc3f794e433fcaed18dd2aa806b3025
i7677264 <i7677264@bournemouth.ac.uk>
2015-03-10 15:19:58 +0000

    Create box.php

---
+++ box.php
@@ -0,0 +1,14 @@
+<?php if(isset($_SESSION["user"])) {?> 
+<div class="box_tweet">
+    <h2><?php echo ucfirst($row["gender"]); ?></h2>
+    <p><?php echo ucfirst($row["tweet"]); ?></p>
+    
+
+
+</body>
+</html>
+
+    
+</div>
+<?php } ?>
+        
