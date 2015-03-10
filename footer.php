cb46dc983d101bdbcb66ca2e7febc1e05485c432
i7677264 <i7677264@bournemouth.ac.uk>
2015-03-10 15:20:22 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>        
+        <script src="js/scripts2.js"></script>
         <script src="js/scripts.js"></script>
     </body>
 </html>
