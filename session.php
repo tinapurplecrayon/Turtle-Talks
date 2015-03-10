c46d2f0e1ff576322754a7bd4ca7e4414750f757
i7677264 <i7677264@bournemouth.ac.uk>
2015-03-10 15:22:44 +0000

    Update session.php

--- session.php
+++ session.php
@@ -1,12 +1,10 @@
 <?php
 	session_start();
-
 	function message() {
 		if(isset($_SESSION["message"])){
 			$output = "<div class=\"container\"><p>";
 			$output .= htmlentities($_SESSION["message"]);
 			$output .= "</p></div>";
-
 			$_SESSION["message"] = null;
 			return $output;
 		}
