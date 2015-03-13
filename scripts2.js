e09fb609b7a31084f6a7091b50ba471fe0804c2c
i7677264 <i7677264@bournemouth.ac.uk>
2015-03-13 12:03:46 +0000

    Update scripts2.js
    the background image slide-part is commented out, tina wanted to look at that and the toggle part is still commented out because it does not work yet

--- scripts2.js
+++ scripts2.js
@@ -9,3 +9,35 @@ $(document).ready(function() {
     });
     
 });
+
+/*$(document).ready(function() {
+    
+    $(".comment_post").hide();
+    
+    $(".comment_bulk").click(function(){
+        console.log("clicked box");
+        
+            $(".comment_post").slideToggle();
+    });
+    
+})*/
+ 
+ /*$(window).load(function() {          
+
+  var i =0;
+  var images = ['a1.jpeg','a2.jpeg','a3.jpeg','a4.jpeg','a5.jpeg','a6.jpeg','a7.jpeg'];
+  var image = $('#slideit');
+                //Initial Background image setup
+  image.css('background-image', 'url(images/a1.jpeg)');
+                //Change image at regular intervals
+  setInterval(function(){  
+   image.fadeOut(1000, function () {
+   image.css('background-image', 'url(' + images [i++] +')');
+   image.fadeIn(1000);
+   });
+   if(i == images.length)
+    i = 0;
+  }, 500);           
+ });
+
+*/
