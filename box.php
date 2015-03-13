0c8e13ad8312108dd975af515cc7534b4f287cda
i7677264 <i7677264@bournemouth.ac.uk>
2015-03-13 12:01:08 +0000

    Update box.php
    update: the comment form is commented out because the jquery does not work yet, it still needs fixing

--- box.php
+++ box.php
@@ -1,10 +1,32 @@
 <?php if(isset($_SESSION["user"])) {?> 
 <div class="box_tweet">
+    
+ <!--<div class="comment_bulk">
+    <a>teeeeeest</a>
+</div> -->  
+    
     <h2><?php echo ucfirst($row["gender"]); ?></h2>
     <p><?php echo ucfirst($row["tweet"]); ?></p>
     
 
+<!--
+<div class="comment_post">
+<form method='post'>
+  Comment:<br />
+  <textarea name='comment' id='comment'></textarea><br />
 
+  <input type='hidden' name='articleid' id='articleid' value='<? echo $_GET["gender"]; ?>' />
+
+  <input type='submit' value='Submit' />  
+</form>
+</div>    
+    
+<div class="comment_bulk">
+    <a>teeeeeest</a>
+</div>
+    
+-->
+    
 </body>
 </html>
 
