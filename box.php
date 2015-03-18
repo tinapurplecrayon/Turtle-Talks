0ac34e121daa225ea76ed84f4926b18c1d149469
Darlene <i7677264@bournemouth.ac.uk>
2015-03-18 16:33:01 +0000

    Update box.php

--- box.php
+++ box.php
@@ -1,36 +1,19 @@
 <?php if(isset($_SESSION["user"])) {?> 
-<div class="box_tweet">
-    
- <!--<div class="comment_bulk">
-    <a>teeeeeest</a>
-</div> -->  
-    
+    <div class="box_tweet">    
+<?php
+    include_once("../includes/functions.php"); // Include the class library
+    $timeAgoObject = new convertToAgo; // Create an object for the time conversion functions
+    // Query your database here and get timestamp
+    $ts = $row["dateofpost"];
+    $convertedTime = ($timeAgoObject -> convert_datetime($ts)); // Convert Date Time
+    $when = ($timeAgoObject -> makeAgo($convertedTime)); // Then convert to ago time
+    ?>
+
+    <div class="time_ago" <h2><?php echo "..".$when; ?></h2> </div>
+        
     <h2><?php echo ucfirst($row["gender"]); ?></h2>
     <p><?php echo ucfirst($row["tweet"]); ?></p>
-    
 
-<!--
-<div class="comment_post">
-<form method='post'>
-  Comment:<br />
-  <textarea name='comment' id='comment'></textarea><br />
-
-  <input type='hidden' name='articleid' id='articleid' value='<? echo $_GET["gender"]; ?>' />
-
-  <input type='submit' value='Submit' />  
-</form>
-</div>    
-    
-<div class="comment_bulk">
-    <a>teeeeeest</a>
-</div>
-    
--->
-    
-</body>
-</html>
-
-    
-</div>
+    </div>
 <?php } ?>
         
