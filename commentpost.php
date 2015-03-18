50b641fad92eb007429f39b630cf5e4833698172
Darlene <i7677264@bournemouth.ac.uk>
2015-03-18 16:35:59 +0000

    Create commentpost.php

---
+++ commentpost.php
@@ -0,0 +1,23 @@
+<?php require_once("sessions.php"); ?>
+<?php require_once("../includes/connect.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+
+<?php
+    /*if(isset($_POST["comment"])) {
+        $comment = $_POST['comment'];
+        if(empty($comment)) {
+            $message = "Invalid comment";
+        } else {
+            $query = "INSERT INTO comments (content, user_id, post_id) VALUES ('{$comment}', '{$_SESSION['user_id']}', '{$_SESSION['post_id']}')"; //the id's are not working, can not access them from the users/post table
+            $result = mysqli_query($connect, $query); 
+            if($result) {
+                $message = "Success, your comment was added!";   
+            } else {
+                $message = "Sorry, something went wrong!"; 
+            }
+            $comment = "";
+            $user_id= "";
+            $post_id= "";
+        }
+    }*/
+?> 
